d66357e4cbf5b8a2ec5a86f8f9ebd3cb853988c4
TylerD0808 <58698514+TylerD0808@users.noreply.github.com>
2020-01-27 08:02:55 -0800

    '

--- Workspace/Chapter6/src/Labs1/Sales.java
+++ Workspace/Chapter6/src/Labs1/Sales.java
@@ -11,26 +11,30 @@ public class Sales
 {
     public static void main(String[] args)
     {
-	final int SALESPEOPLE = 5;
-	int[] sales = new int[SALESPEOPLE];
-	int sum, average;
+		Scanner scan = new Scanner(System.in);
 
-	Scanner scan = new Scanner(System.in);
+		System.out.print("How many salespeople? ");
+		int numSalespeople = scan.nextInt();
 
-	for (int i=0; i<sales.length; i++)
-	    {
-		System.out.print("Enter sales for salesperson " + i + ": ");
-		sales[i] = scan.nextInt();
-	    }
+		int[] sales = new int[numSalespeople];
+		int sum, average;
+		
+		for (int i = 0; i < sales.length; i++)
+		{
+			System.out.print("Enter sales for salesperson " + (1 + i) + ": ");
+			sales[i] = scan.nextInt();
+		}
 
-	System.out.println("\nSalesperson   Sales");
-	System.out.println("--------------------");
-	sum = 0;
-	int highestSale = sales[0], salesPerson = 0, lowestSale = sales[0], salesPerson2 = 0;
-	for (int i=0; i<sales.length; i++)
-	    {
-		System.out.println("     " + i + "         " + sales[i]);
-		sum += sales[i];
+		System.out.println("\nSalesperson   Sales");
+		System.out.println("--------------------");
+
+		sum = 0;
+
+		int highestSale = sales[0], salesPerson = 0, lowestSale = sales[0], salesPerson2 = 0;
+		for (int i = 0; i < sales.length; i++)
+		{
+			System.out.println("     " + (1 + i) + "         " + sales[i]);
+			sum += sales[i];
 
 			if (sales[i] > highestSale)
 			{
@@ -44,11 +48,34 @@ public class Sales
 				salesPerson2 = i;
 			}
 		}
-		
-		average = sum/SALESPEOPLE;
+			
+		average = sum / numSalespeople;
 
-	System.out.println("\nTotal sales: " + sum);
-	System.out.println("Average sale: " + average);
-	System.out.println("Salesperson " + salesPerson + " had the highest sale with $" + highestSale);
+		System.out.println("\nTotal sales: " + sum);
+		System.out.println("Average sale: " + average);
+		System.out.println("Salesperson " + (1 + salesPerson) + " had the highest sale with $" + highestSale);
+		System.out.println("Salesperson " + (1 + salesPerson2) + " had the lowest sale with $" + lowestSale);
+		System.out.println();
+		System.out.print("Please enter a value: ");
+		int val = scan.nextInt();
+		int g = 0;
+		System.out.println("Saleseople who exceeded $" + val + " in sales:");
+		System.out.println("\nSalesperson   Sales");
+		System.out.println("--------------------");
+
+		for (int k = 0; k < sales.length; k++)
+		{
+			if (sales[k] > val)
+			{
+				System.out.println("     " + (1 + k) + "         " + sales[k]);
+				g++;
+			}
+		}
+
+		System.out.println(g + " salespeople exceeded $" + val + " in sales");
+	
+		
+
+
     }
 }
