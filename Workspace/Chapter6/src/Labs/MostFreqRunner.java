946b381ba219de4d14a909d39bc41b8f99f95535
TylerD0808 <58698514+TylerD0808@users.noreply.github.com>
2020-01-30 08:29:42 -0800

    Frequency 1

---
+++ Workspace/Chapter6/src/Labs/MostFreqRunner.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class MostFreqRunner {
+    public static void main (String[]args) {
+        ArrayList<Integer> listA = new ArrayList<Integer>(
+            Arrays.asList(-99, 1, 2, 3, 4, 5, 6, 6, 6, 6, 6, 7, 8, 9, 10, 12345, 5, 5, 5, 5));
+        ArrayList<Integer> listB = new ArrayList<Integer>(
+            Arrays.asList(10,9,8,7,6,5,4,3,2,1,-99));
+        ArrayList<Integer> listC = new ArrayList<Integer>(
+            Arrays.asList(10,20,30,40,50,10,10,40,30,20,10));
+        ArrayList<Integer> listD = new ArrayList<Integer>(
+            Arrays.asList(32767));
+        ArrayList<Integer> listE = new ArrayList<Integer>(
+            Arrays.asList(255,255));
+        ArrayList<Integer> listF = new ArrayList<Integer>(
+            Arrays.asList(9,10,-88,100,-555,1000));
+        ArrayList<Integer> listG = new ArrayList<Integer>(
+            Arrays.asList(10,10,10,11,456,10,10,2,2,2,2,2,2,2));
+        ArrayList<Integer> listH = new ArrayList<Integer>(
+            Arrays.asList(-111,1,2,3,9,11,20,30));
+        ArrayList<Integer> listI = new ArrayList<Integer>(
+            Arrays.asList(9,8,7,6,5,4,3,2,0,-2,-989));
+        ArrayList<Integer> listJ = new ArrayList<Integer>(
+            Arrays.asList(12,12,15,18,21,23,1000));
+        ArrayList<Integer> listK = new ArrayList<Integer>(
+            Arrays.asList(250,19,17,15,13,13,13,13,11,10,9,6,3,2,1,1));
+        ArrayList<Integer> listL = new ArrayList<Integer>(
+            Arrays.asList(9,10,-8,10000,-5000,1000));
+
+    }
+}
