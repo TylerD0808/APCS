a4942d3d7ec64b9f15c1f244acba74a195bb59b5
TylerD0808 <tyler.debrock@yahoo.com>
2020-03-18 22:50:41 -0700

    Digit Play

--- Workspace/Chapter8/src/Labs/Power.java
+++ Workspace/Chapter8/src/Labs/Power.java
@@ -7,35 +7,32 @@
 
 import java.util.Scanner;
 
-public class Power
-{
-    public static void main(String[] args)
-    {
-	int base, exp;
-	int answer;
+public class Power {
+	public static void main(String[] args) {
+		int base, exp;
+		int answer;
 
-	Scanner scan = new Scanner(System.in);
+		Scanner scan = new Scanner(System.in);
 
-	System.out.print("Welcome to the power program! ");
-	System.out.println("Please use integers only.");
-	
-	//get base
-	System.out.print("Enter the base you would like raised to a power: ");
-	base = scan.nextInt();
+		System.out.print("Welcome to the power program! ");
+		System.out.println("Please use integers only.");
 
-	//get exponent
-	System.out.print("Enter the power you would like it raised to: ");
-	exp = scan.nextInt();
+		// get base
+		System.out.print("Enter the base you would like raised to a power: ");
+		base = scan.nextInt();
 
-	answer = power(base,exp);
-	System.out.println(base + " raised to the " + exp + " is " + answer);
-    }
+		// get exponent
+		System.out.print("Enter the power you would like it raised to: ");
+		exp = scan.nextInt();
 
-    // -------------------------------------------------
-    //   Computes and returns base^exp
-    // -------------------------------------------------
-    public static int power(int base, int exp)
-    {
+		answer = power(base, exp);
+		System.out.println(base + " raised to the " + exp + " is " + answer);
+	}
+
+	// -------------------------------------------------
+	// Computes and returns base^exp
+	// -------------------------------------------------
+	public static int power(int base, int exp) {
 		int pow;
 
 		if (exp == 0) {
@@ -45,5 +42,5 @@ public class Power
 		}
 
 		return pow;
-    }
+	}
 }
