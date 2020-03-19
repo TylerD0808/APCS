c7944a7f3dfa4318e68db47b41a29e4a0e90125e
TylerD0808 <tyler.debrock@yahoo.com>
2020-03-18 23:54:50 -0700

    Fibonacci

--- Workspace/Chapter8/src/Labs/Fib.java
+++ Workspace/Chapter8/src/Labs/Fib.java
@@ -4,16 +4,30 @@
 //   A utility class that provide methods to compute elements of the
 //   Fibonacci sequence.
 // ******************************************************************
-public class Fib
-{
+public class Fib {
 
-    //--------------------------------------------------------------
+    // --------------------------------------------------------------
     // Recursively computes fib(n)
-    //--------------------------------------------------------------
-    public static int fib1(int n)
-    {
-	//Fill in code -- this should look very much like the
-	//mathematical specification
+    // --------------------------------------------------------------
+    public static int fib1(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return fib1(n - 1) + fib1(n - 2);
+        }
     }
 
+    public static int fib2(int n) {
+        int[] array = new int[n + 1];
+        array[0] = 0;
+        array[1] = 1;
+
+        for (int k = 2; k < n + 1; k++) {
+            array[k] = array[k - 1] + array[k - 2];
+        }
+
+        return array[n];
+    }
 }
--- Workspace/Chapter8/src/Labs/TestFib.java
+++ Workspace/Chapter8/src/Labs/TestFib.java
@@ -7,18 +7,19 @@
 
 import java.util.Scanner;
 
-public class TestFib
-{
-    public static void main(String[] args)
-    {
-	int n, fib;
+public class TestFib {
+	public static void main(String[] args) {
+		int n, fib;
 
-	Scanner scan = new Scanner(System.in);
+		Scanner scan = new Scanner(System.in);
 
-	System.out.print("Enter an integer: ");
-	n = scan.nextInt();
+		System.out.print("Enter an integer: ");
+		n = scan.nextInt();
 
-	fib = Fib.fib1(n);
-	System.out.println("Fib(" + n + ") is " + fib);
-    }
-} 
+		fib = Fib.fib2(n);
+		System.out.println("Fib2(" + n + ") is " + fib);
+
+		fib = Fib.fib1(n);
+		System.out.println("Fib1(" + n + ") is " + fib);
+	}
+}
