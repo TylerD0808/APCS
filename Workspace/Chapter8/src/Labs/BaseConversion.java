ec1d654c755913d20a8425255e26b55c2607c118
TylerD0808 <tyler.debrock@yahoo.com>
2020-03-18 23:07:51 -0700

    BaseConversion

--- Workspace/Chapter8/src/Labs/BaseConversion.java
+++ Workspace/Chapter8/src/Labs/BaseConversion.java
@@ -6,32 +6,37 @@
 
 import java.util.Scanner;
 
-public class BaseConversion
-{
-    public static void main (String[] args)
-    {
-	int base10Num;
-	int base;
+public class BaseConversion {
+	public static void main(String[] args) {
+		int base10Num;
+		int base;
 
-	Scanner scan = new Scanner(System.in);
+		Scanner scan = new Scanner(System.in);
 
-	System.out.println ();
-	System.out.println ("Base Conversion Program");
-	System.out.print ("Enter an integer: ");
-	base10Num = scan.nextInt();
-	System.out.print ("Enter the base: ");
-	base = scan.nextInt();
+		System.out.println();
+		System.out.println("Base Conversion Program");
+		System.out.print("Enter an integer: ");
+		base10Num = scan.nextInt();
+		System.out.print("Enter the base: ");
+		base = scan.nextInt();
 
-	// Call convert and print the answer
-	
-    }
-    // --------------------------------------------------
-    //   Converts a base 10 number to another base.
-    // -------------------------------------------------- 
-    public static String convert (int num, int b)
-    {
-	int quotient;  // the quotient when num is divided by base b
-	int remainder; // the remainder when num is divided by base b
-    }
+		System.out.println(base10Num + " converted to base " + base + " is " + convert(base10Num, base));
+	}
+
+	// --------------------------------------------------
+	// Converts a base 10 number to another base.
+	// --------------------------------------------------
+	public static String convert(int num, int b) {
+		int quotient; // the quotient when num is divided by base b
+		int remainder; // the remainder when num is divided by base b
+
+		quotient = num / b;
+		remainder = num % b;
+		if (quotient != 0) {
+			return convert(quotient, b) + remainder;
+		} else {
+			return "" + remainder;
+		}
+	}
 
 }
