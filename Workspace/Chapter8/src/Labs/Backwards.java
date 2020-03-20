691b5f80caaa7c953e49823d0ffc412b2165949b
TylerD0808 <tyler.debrock@yahoo.com>
2020-03-19 17:23:01 -0700

    Backwards

--- Workspace/Chapter8/src/Labs/Backwards.java
+++ Workspace/Chapter8/src/Labs/Backwards.java
@@ -1,3 +1,4 @@
+
 // ******************************************************************
 //   Backwards.java
 //
@@ -5,32 +6,32 @@
 // ******************************************************************
 import java.util.Scanner;
 
-public class Backwards
-{
+public class Backwards {
 
-    //--------------------------------------------------------------
+    // --------------------------------------------------------------
     // Reads a string from the user and prints it backwards.
-    //--------------------------------------------------------------
-    public static void main(String[] args)
-    {
-	String msg;
-	Scanner scan = new Scanner(System.in);
+    // --------------------------------------------------------------
+    public static void main(String[] args) {
+        String msg;
+        Scanner scan = new Scanner(System.in);
 
-	System.out.print("Enter a string: ");
-	msg = scan.nextLine();
+        System.out.print("Enter a string: ");
+        msg = scan.nextLine();
 
-	System.out.print("\nThe string backwards: ");
-	printBackwards(msg);
-	System.out.println();
+        System.out.print("\nThe string backwards: ");
+        printBackwards(msg);
+        System.out.println();
     }
-	
-    //--------------------------------------------------------------
+
+    // --------------------------------------------------------------
     // Takes a string and recursively prints it backwards.
-    //--------------------------------------------------------------
-    public static void printBackwards(String s)
-    {
-
-	// Fill in code
-
+    // --------------------------------------------------------------
+    public static void printBackwards(String s) {
+        if (s.length() != 0) {
+            System.out.print(s.charAt(s.length() - 1));
+            printBackwards(s.substring(0, s.length() - 1));
+        } else {
+            System.out.print("");
+        }
     }
 }
