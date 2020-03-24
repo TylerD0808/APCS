1396fbddbc4928332c83d42527c27c171dedb9b5
TylerD0808 <tyler.debrock@yahoo.com>
2020-03-23 17:23:54 -0700

    Chatbot Activity2

--- Workspace/Other/Magpie Chatbot/activity2/Magpie2.java
+++ Workspace/Other/Magpie Chatbot/activity2/Magpie2.java
@@ -1,80 +1,87 @@
 /**
- * A program to carry on conversations with a human user.
- * This is the initial version that:  
- * <ul><li>
- *       Uses indexOf to find strings
- * </li><li>
- * 		    Handles responding to simple words and phrases 
- * </li></ul>
+ * A program to carry on conversations with a human user. This is the initial
+ * version that:
+ * <ul>
+ * <li>Uses indexOf to find strings</li>
+ * <li>Handles responding to simple words and phrases</li>
+ * </ul>
  * This version uses a nested if to handle default responses.
+ * 
  * @author Laurie White
  * @version April 2012
  */
-public class Magpie2
-{
+public class Magpie2 {
 	/**
-	 * Get a default greeting 	
+	 * Get a default greeting
+	 * 
 	 * @return a greeting
 	 */
-	public String getGreeting()
-	{
+	public String getGreeting() {
 		return "Hello, let's talk.";
 	}
-	
+
 	/**
 	 * Gives a response to a user statement
 	 * 
-	 * @param statement
-	 *            the user statement
+	 * @param statement the user statement
 	 * @return a response based on the rules given
 	 */
-	public String getResponse(String statement)
-	{
+	public String getResponse(String statement) {
 		String response = "";
-		if (statement.indexOf("no") >= 0)
-		{
+
+		statement.trim();
+
+		if (statement.length() == 0) {
+			response = "Please say something";
+		} else if (statement.indexOf("Mr.") >= 0) {
+			response = "He sounds like a good teacher";
+		} else if (statement.indexOf("Mrs.") >= 0) {
+			response = "She sounds like a great teacher";
+		} else if (statement.indexOf("no") >= 0) {
 			response = "Why so negative?";
-		}
-		else if (statement.indexOf("mother") >= 0
-				|| statement.indexOf("father") >= 0
-				|| statement.indexOf("sister") >= 0
-				|| statement.indexOf("brother") >= 0)
-		{
+		} else if (statement.indexOf("mother") >= 0 || statement.indexOf("father") >= 0) {
 			response = "Tell me more about your family.";
-		}
-		else
-		{
+		} else if (statement.indexOf("brother") >= 0) {
+			// edit to brother
+			response = "Is your brother older or younger?";
+		} else if (statement.indexOf("sister") >= 0) {
+			// edit to sister
+			response = "Do you ever fight with her?";
+		} else if (statement.indexOf("dog") >= 0) {
+			response = "Tell me more about your pets.";
+		} else if (statement.indexOf("cat") >= 0) {
+			// edit to cat
+			response = "Do you prefer dogs or cats?";
+		} else {
 			response = getRandomResponse();
 		}
+
 		return response;
 	}
 
 	/**
 	 * Pick a default response to use if nothing else fits.
+	 * 
 	 * @return a non-committal string
 	 */
-	private String getRandomResponse()
-	{
-		final int NUMBER_OF_RESPONSES = 4;
+	private String getRandomResponse() {
+		final int NUMBER_OF_RESPONSES = 6;
 		double r = Math.random();
-		int whichResponse = (int)(r * NUMBER_OF_RESPONSES);
+		int whichResponse = (int) (r * NUMBER_OF_RESPONSES);
 		String response = "";
-		
-		if (whichResponse == 0)
-		{
+
+		if (whichResponse == 0) {
 			response = "Interesting, tell me more.";
-		}
-		else if (whichResponse == 1)
-		{
+		} else if (whichResponse == 1) {
 			response = "Hmmm.";
-		}
-		else if (whichResponse == 2)
-		{
+		} else if (whichResponse == 2) {
 			response = "Do you really think so?";
-		}
-		else if (whichResponse == 3)
-		{
+		} else if (whichResponse == 3) {
 			response = "You don't say.";
+		} else if (whichResponse == 4) {
+			response = "Wow that's so cool.";
+		} else if (whichResponse == 5) {
+			response = "Is that fun?";
 		}
 
 		return response;
