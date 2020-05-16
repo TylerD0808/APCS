05d0f9aff3dad83bfde8f3e2b278bd421ac18a0f
TylerD0808 <tyler.debrock@yahoo.com>
2020-05-15 17:19:39 -0700

    AP Exam

--- Workspace/AP Test/OfficialAPExam.java
+++ Workspace/AP Test/OfficialAPExam.java
@@ -14,7 +14,27 @@ Part A
 */
 
 
+public double getCompoundAvg(String comp) {
 
+    private int x = 0;
+    private int total = 0;
+    private double avg = -1.0;
+
+    for (int i = 0; i < trialList.size(); i++) {
+        Trial t = trialList.get(i);
+        if (t.getCompound().equals(comp))
+        {
+            total += t.getTemp();
+            x++;
+        }
+    }
+
+    if (x != 0) {
+        avg = ((double)total) / x;
+    }
+
+    return avg;
+}
 
 
 /*
@@ -22,7 +42,18 @@ Part B
 */
 
 
+    public String getCompoundWithHigheestAvg() {
 
+        private String c = getCompoundList.get(0);
+
+        for (int i = 1; i < getCompoundList.size(); i++) {
+           if (getCompoundAvg(getCompoundList.get(i) >= getCompoundAvg(c))) {
+               c = getCompoundList.get(i);
+           }
+        }
+
+        return c;
+    }
 
 
 /*
@@ -30,8 +61,16 @@ Part C
 */
 
 
-
-
+    //Method header:
+    public double getCompoundAmountUsed(String comp)
+    //I would add a new private instance variable
+    private double amountUsed;
+    //into Trial. This would be instantiated in the new Trial constructor. This new
+    //constructor will contain another parameter, which takes in a double, representing
+    //the amount of that compound used. I would also add a
+    public double getAmountUsed()
+    //method, which returns the value of the amount used instance variable. No changes would
+    //be made to the science experiment class to support this modification.
 
     }
 }
--- Workspace/AP Test/OfficialAPExam2.java
+++ Workspace/AP Test/OfficialAPExam2.java
@@ -14,15 +14,48 @@ Part A
 */
 
 
+    public static boolean runSimulation(int sampleSize) {
 
+        private int x = 0;
+        private int x1;
+        private int y = 0;
+        private int y1;
+
+        for (int i = 0; i < sampleSize / 2; i++) {
+            x1 = getInt();
+            if (isTarget(x1) && x1 > 0) {
+                x++;
+            }
+        }
+
+        for (int i = sampleSize / 2; i < sampleSize; i++) {
+            y1 = getInt();
+            if (isTarget(y1) && y1 > 0) {
+                y++;
+            }
+        }
+
+        if (x > y) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
 
 
 /*
 Part B
 */
 
-
-
+    //I would start off by adding a new
+    private static int sampleSize;
+    //instance variable. I would add another TargetIntegers class constructor,    
+    //which takes in a positive, even int parameter, so the value of sampleSize
+    //could be set by the person. I would also add 
+    sampleSize = 6;
+    //to the default constructor, just in case. This value would feed in directly to
+    //the runSimulation parameter.
 
 
 /*
